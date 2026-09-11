0b6c9b1821cfca2783b0b5a7e2eab9f5f4c487b7
Anton Nordström <an225ga@student.lnu.se>
2026-09-11 13:48:04 +0200

    Add game to main

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@
  * Main module.
  */
 
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 import { getLine } from './console-input.js'
+import Game from './game.js'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const main = async () => {
-  console.log('På spåret')
-
-  const inp = await getLine('test input:\n')
-  console.log(inp)
+  const game = new Game(path.join(__dirname, 'questions.json'))
+  game.play()
 }
 
 main()
