c64750b6c594a057446e10a52967c579be1d5149
Edwin Johnsson <ej224wy@student.lnu.se>
2026-09-11 11:53:35 +0200

    Create basic player

---
+++ src/player.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+class Player {
+  /**
+   * @type {string}
+   */
+  #name
+
+  /**
+   * @type {number}
+   */
+  #points
+
+  constructor (name) {
+    this.#name = name.trim()
+    this.#points = 0
+  }
+
+  /**
+   * Gets the players name.
+   * 
+   * @returns {string} The name.
+   */
+  getName () {
+    return this.#name
+  }
+
+  /**
+   * Adds points to the player.
+   * 
+   * @param {number} points Number of points to add
+   */
+  addPoints (points) {
+    this.#points += points
+  }
+
+  /**
+   * Gets tha players points.
+   * 
+   * @returns {number} The players current points.
+   */
+  getPoints () {
+    return this.#points
+  }
+}
+
+export default Player
